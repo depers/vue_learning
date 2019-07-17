96ca59e63fe7de69fe80aa3a828aa1a9cee3710e
depers <dev_fengxiao@163.com>
2019-07-17 21:47:53 +0800

    project init, 04 chapter

---
+++ hello-world/src/App1.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <ol>
+            <todo-item v-for="item in groceryList" v-bind:todo="item" v-bind="item.id"></todo-item>
+        </ol>
+    </div>
+</template>
+
+<script lang="ts">
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+    name: 'app',
+    data(){
+        return {
+            groceryList: [{
+                    id: 0,
+                    text: '蔬菜'
+                },{
+                    id: 1,
+                    text: '奶酪'
+                },{
+                    id: 2,
+                    text: '好吃的'
+            }]
+        }
+    },
+    components: {
+        TodoItem
+    }
+}
+</script>
